f997d3c8eb0128afe1f8b7918914f678767b2ae7
Eongyu Park <peg5325@gmail.com>
2023-05-24 16:39:43 +0900

    add HandlerAdapter 여기까지가 전반적인 mvc framework flow를 이해를 위한 구현

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -3,6 +3,7 @@ package org.example.mvc;
 import org.example.mvc.controller.Controller;
 import org.example.mvc.controller.RequestMethod;
 import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.ModelAndView;
 import org.example.mvc.view.View;
 import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
@@ -25,14 +26,17 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private List<ViewResolver> viewResolvers;
     private RequestMappingHandlerMapping rmhm;
+
+    private List<HandlerAdapter> handlerAdapters;
+    private List<ViewResolver> viewResolvers;
 
     @Override
     public void init() throws ServletException {
         rmhm = new RequestMappingHandlerMapping();
         rmhm.init();
 
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter());
         viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
@@ -42,12 +46,21 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             Controller handler = rmhm.findHandler(new HandlerKey(RequestMethod.valueOf(request.getMethod()), request.getRequestURI()));
-            String viewName = handler.handleRequest(request, response);
+
+            // handlerAdapter가 생겨서 이 역할을 대신함. 고로 밑에 줄 필요x
+            // String viewName = handler.handleRequest(request, response);
+
+            HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                    .filter(ha -> ha.supports(handler))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No adapter for handler [" + handler + "]"));
+
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
 
 //            "redirect" vs "forward"
             for (ViewResolver viewResolver : viewResolvers) {
-                View view = viewResolver.resolveView(viewName);
-                view.render(new HashMap<>(), request, response);
+                View view = viewResolver.resolveView(modelAndView.getViewName());
+                view.render(modelAndView.getModel(), request, response);
             }
 
         } catch (Exception e) {
